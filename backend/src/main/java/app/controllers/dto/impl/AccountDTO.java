96a3a7d5a6bae8b66562200b34a6bc25511b6b9b
Nezumish <monochromeworld42@gmail.com>
2021-07-09 20:51:36 +0400

    Доделаны ДТО продукта и характеристик

--- backend/src/main/java/app/controllers/dto/impl/AccountDTO.java
+++ backend/src/main/java/app/controllers/dto/impl/AccountDTO.java
@@ -8,8 +8,8 @@ import lombok.Data;
 import lombok.Value;
 
 /**
- * Общий енам для ДТО объектов класса продукта
- * Содержит енамы для Запроса и Ответа
+ * Содержит енамы классов, отвечающих за
+ * запросы и ответы по аккаунту пользователя
  */
 public enum AccountDTO implements DTO {;
 
@@ -17,10 +17,16 @@ public enum AccountDTO implements DTO {;
         Integer getId();
     }
 
+    /**
+     * Что-то для Outh2
+     */
     private interface Token {
         String getToken();
     }
 
+    /**
+     * Для определения прав пользователя
+     */
     private interface Rights {
         String getRights();
     }
@@ -39,11 +45,17 @@ public enum AccountDTO implements DTO {;
 
     public enum Request {;
 
+        /**
+         * Получение пользователя по его ID
+         */
         @Data
         public static class Get implements Id {
             private Integer id;
         }
 
+        /**
+         * Используется при регистрации пользователя
+         */
         @Data
         public static class Create implements Rights, Email, FullName, Address, Token {
             private String rights;
@@ -53,24 +65,36 @@ public enum AccountDTO implements DTO {;
             private String token;
         }
 
+        /**
+         * Запрос пользователя на изменение имени
+         */
         @Data
         public static class UpdateName implements Id, FullName {
             private Integer id;
             private String fullName;
         }
 
+        /**
+         * Запрос пользователя на изменение почты
+         */
         @Data
         public static class UpdateEmail implements Id, Email {
             private Integer id;
             private String email;
         }
 
+        /**
+         * Запрос пользователя на изменение адреса
+         */
         @Data
         public static class UpdateAddress implements Id, Address {
             private Integer id;
             private String address;
         }
 
+        /**
+         * Запрос пользователя на удаление своего аккаунта
+         */
         @Data
         public static class Delete implements Id {
             private Integer id;
@@ -79,12 +103,18 @@ public enum AccountDTO implements DTO {;
 
     public enum Response {;
 
+        /**
+        * Основная информация о пользователе
+        */
         @Value
         public static class GetShortInfo implements Email, FullName {
             String email;
             String fullName;
         }
 
+        /**
+         * Полная информация о пользователе
+         */
         @Value
         public static class GetFullInfo implements Id, Rights, Email, FullName, Token, Address {
             Integer id;
--- backend/src/main/java/app/controllers/dto/impl/FeatureDTO.java
+++ backend/src/main/java/app/controllers/dto/impl/FeatureDTO.java
@@ -1,5 +1,5 @@
 /*
- * Uliana
+  Uliana
  */
 package app.controllers.dto.impl;
 
@@ -7,7 +7,12 @@ import app.controllers.dto.DTO;
 import lombok.Data;
 import lombok.Value;
 
+/**
+ * Содержит енамы классов, отвечающих за
+ * запросы и ответы по характеристикам конкретного продукта
+ */
 public enum FeatureDTO implements DTO {;
+
     private interface Id {
         Integer getId();
     }
@@ -16,10 +21,28 @@ public enum FeatureDTO implements DTO {;
         String getName();
     }
 
-    private interface Units {
+    /**
+     * Содержит информацию о измеряемой
+     * характеристики продукта
+     * для клиента
+     */
+    private interface ResponseUnits {
         UnitsDTO.Response.Get getUnits();
     }
 
+    /**
+     * Содержит информацию об изменении
+     * измеряемой характеристики продукта
+     * для сервера
+     */
+    private interface ChangeUnits {
+        UnitsDTO.Request.ChangeUnits getChangesUnits();
+    }
+
+    /**
+     * Ссодержит значение безмерочной
+     * характеристики продукта
+     */
     private interface FeatureValue {
         String getFeatureValue();
     }
@@ -30,32 +53,67 @@ public enum FeatureDTO implements DTO {;
 
     public enum Request {;
 
+        /**
+        * Для получения характеристики с сервера по ее ID
+        */
         @Data
         public static class Get implements Id {
             private Integer id;
         }
 
+        /**
+         * Для изменения мерочной характеристики товара по ее ID
+         * на новое значение
+         */
         @Data
-        public static class UpdateUnit
+        public static class UpdateUnits implements Id, ChangeUnits {
+            private Integer Id;
+            private UnitsDTO.Request.ChangeUnits changesUnits;
+        }
 
+        /**
+         * Для изменнеия безмерочной характеристики товара по ее ID
+         * на новое значение
+         */
+        @Data
+        public static class UpdateValue implements Id, FeatureValue {
+            private Integer Id;
+            private String featureValue;
+        }
+
+        /**
+         * Для удаления характеристики товара по ее ID
+         */
+        @Data
+        public static class Delete implements Id {
+            private Integer Id;
+        }
 
     }
 
     public enum Response {;
+        /**
+         * Возвращает информацию о мерочной характеристике
+         * продукта
+         */
         @Value
-        public static class GetWithUnits implements Name, FeaturePrice, Units {
+        public static class GetWithUnits implements Name, FeaturePrice, ResponseUnits {
             String name;
             UnitsDTO.Response.Get units;
             Integer featurePrice;
         }
 
+        /**
+         * Возвращает информацию о безмерочной характеристике
+         * продукта
+         */
         @Value
-        public static class Get implements Name, FeaturePrice {
+        public static class GetUnitless implements Name, FeaturePrice, FeatureValue {
             String name;
+            String featureValue;
             Integer featurePrice;
         }
 
     }
-
 
 }
