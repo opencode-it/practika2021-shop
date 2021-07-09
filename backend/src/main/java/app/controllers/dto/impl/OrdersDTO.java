ce07b2246b027098509f178b70b925fe59c83af6
Nezumish <monochromeworld42@gmail.com>
2021-07-09 21:43:34 +0400

    Доделан класс ДТО заказа

--- backend/src/main/java/app/controllers/dto/impl/OrdersDTO.java
+++ backend/src/main/java/app/controllers/dto/impl/OrdersDTO.java
@@ -1,38 +1,156 @@
+/*
+* Uliana
+*/
 package app.controllers.dto.impl;
 
-import app.entities.Product;
+import app.controllers.dto.DTO;
+import lombok.Data;
+import lombok.Value;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+/**
+ * Содержит енамы классов, отвечающих за
+ * запросы и ответы по заказу
+ */
 public enum OrdersDTO {;
 
-    private interface Account {
-        AccountDTO.Request.Get getAccount();
+    /**
+     * ID аккаунта заказчика
+     */
+    private interface AccountId {
+        Integer getAccountId();
     }
 
- //   private interface Products
-
-    private interface Count {
-        Integer getCount();
+    /**
+     * ID заказа
+     */
+    private interface Id {
+        Integer getId();
     }
-/*
 
+    /**
+     * Продукты и их количество в заказе
+     */
+    private interface ProductsId {
+        Map<Integer, Integer> getOrderedProductIds();
+    }
+
+    /**
+     * Для обращения к одному продукту в заказе
+     */
+    private interface Product {
+        Integer getProductId();
+    }
+
+    /**
+     * Короткая информация о всех продуктах в заказе
+     */
+    private interface ProductsInfo {
+        Set<ProductDTO.Response.GetBase> getProductsInOrder();
+    }
+
+    /**
+     * Примененная скидка к заказу
+     */
     private interface Discount {
-        DiscountDTO getDiscount();
+        Integer getDiscount();
     }
-*/
 
+    /**
+     * Дата оформления заказа
+     */
     private interface Date {
         LocalDateTime getDate();
     }
 
     /**
-     * дописать
-     * */
+     * Текуший статус заказа
+     */
+    private interface Status {
+        String getStatus();
+    }
 
-    // private interface OrderStatus
+    /**
+     * Полная стоимость заказа
+     */
+    private interface GrandTotal {
+        BigDecimal getGrandTotal();
+    }
 
+    public enum Request {;
 
+        /**
+         * Запрос на получение информации по одному закзу
+         */
+        @Data
+        public static class Get implements DTO, Id {
+            private Integer id;
+        }
 
+        /**
+         * Запрос на уменьшение конкретного продукта в заказе на одну единицу
+         */
+        @Data
+        public static class DecreaseProductAmount implements DTO, Id, Product {
+            private Integer Id;
+            private Integer productId;
+        }
+
+        /**
+         * Запрос на увеличение конкретного продукта в заказе на одну единицу
+         */
+        @Data
+        public static class IncreaseProductAmount implements DTO, Id, Product {
+            private Integer Id;
+            private Integer productId;
+        }
+
+        /**
+         * Запрос на применение скидки к заказу
+         */
+        @Data
+        public static class ApplyDiscount implements DTO, Id, Discount {
+            //TODO и че с этим делать
+        }
+
+        /**
+         * Запрос на формирование заказа из товаров в корзине
+         */
+        @Data
+        public static class ConfirmOrder implements DTO, AccountId, ProductsId {
+            private Integer accountId;
+            private Map<Integer, Integer> orderedProductIds;
+        }
+    }
+
+    public enum Response {;
+
+        /**
+        * Запрос на отображение корзины перед окончательным оформлением заказа в системе
+        */
+        @Value
+        public static class GetFormed implements DTO, Id, AccountId, ProductsInfo, GrandTotal {
+            Integer id;
+            Integer accountId;
+            Set<ProductDTO.Response.GetBase> productsInOrder;
+            BigDecimal grandTotal;
+        }
+
+        /**
+         * Запрос на отображение полной информации о совершенном заказе
+         */
+        @Value
+        public static class GetFull implements DTO, Id, AccountId, ProductsInfo, GrandTotal, Date, Status {
+            Integer id;
+            Integer accountId;
+            Set<ProductDTO.Response.GetBase> productsInOrder;
+            BigDecimal grandTotal;
+            LocalDateTime date;
+            String status;
+        }
+    }
 }
