252a61e2c6e624632bd70a62b9d10591ce0512be
owlonaf <imviktor0170@gmail.com>
2021-07-09 19:32:59 +0400

    Corrected SlotDTO

--- backend/src/main/java/app/controllers/dto/SlotDTO.java
+++ backend/src/main/java/app/controllers/dto/SlotDTO.java
@@ -5,29 +5,50 @@ import lombok.Value;
 
 public enum SlotDTO {;
 
-    private interface Product{
+    private interface Id {
+        Integer getId();
+    }
+
+    private interface Product {
         String getProduct();
     }
 
-    private interface Amount{
+    private interface Amount {
         Integer getAmount();
     }
 
     public enum Request {;
 
-    @Data
-    public static class Get implements Product{
-         private String product;
+        @Data
+        public static class Get implements Id {
+            private Integer id;
+        }
+
+        @Data
+        public static class SetAmount implements Product, Amount {
+            private String product;
+            private Integer amount;
+        }
+
+        @Data
+        public static class SetAmountOnId implements Id, Amount {
+            private String id;
+            private Integer amount;
+        }
     }
 
-    }
+    public enum Response {;
 
-    public enum Response{;
+        @Value
+        public static class GetAmountOnId implements Amount, Id {
+            Integer id;
+            Integer amount;
+        }
 
-    @Value
-        public static class GetAmount implements Amount{
-        Integer amount;
-    }
-
+        @Value
+        public static class GetFullInfo implements Amount, Product {
+            Integer amount;
+            String product;
+        }
     }
 }
