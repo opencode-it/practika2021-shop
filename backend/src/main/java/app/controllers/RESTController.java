f4980d1613f6c37cc2609b135bd4b7ca8a675cbe
Ilya <180229@edu.psuti.ru>
2021-07-09 20:18:30 +0400

    Add generics to RESTController

--- backend/src/main/java/app/controllers/RESTController.java
+++ backend/src/main/java/app/controllers/RESTController.java
@@ -1,5 +1,11 @@
 package app.controllers;
 
-public abstract class RESTController {
+import app.controllers.dto.DTO;
+import app.services.AbstractService;
+
+public abstract class RESTController<D extends DTO, S extends AbstractService<?, D, ?, ?>> {
+
+    S service;
+
 
 }
