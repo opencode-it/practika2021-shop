0e9bc64af9f128b2e1ff404f932e39c55877168c
Tair2033 <63341187+Tair2033@users.noreply.github.com>
2021-07-10 00:57:54 +0400

    product card

---
+++ frontend/src/components/AppProductCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <router-link to="/" class="flex-item">
+    <img :src="item.link" class="item" />
+    <span class="product-name">{{ item.name.substring(0, 12) }}</span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.product-name{
+  display: block;
+  font-style: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 22px 24px;
+  color: black;
+}
+
+
+.item {
+  position: relative;
+  top: -2px;
+  width: 100%;
+  height: auto;
+}
+
+.flex-item {
+  position: relative;
+  background-color: #e0dad2;
+  border-radius: 12px;
+  margin-left: 20px;
+  text-decoration: none;
+}
+
+.flex-item:hover {
+  background-color: #e79247;
+  box-shadow: -1px 1px 14px 19px rgba(34, 60, 80, 0.2);
+}
+</style>
--- frontend/src/components/TheCatalogContent.vue
+++ frontend/src/components/TheCatalogContent.vue
@@ -1,92 +1,55 @@
 <template>
   <div class="content">
     <div class="accordions">
-      <button class="accordion" @click="move(0)">
-        Размер<span class="accordion-arrow">&lsaquo;</span>
-      </button>
-      <div class="panel">
-        <p>Lorem ipsum...</p>
-      </div>
-
-      <button class="accordion" @click="move(1)">
-        Основной цвет<span class="accordion-arrow">&lsaquo;</span>
-      </button>
-      <div class="panel">
-        <p>Lorem ipsum...</p>
-      </div>
-
-      <button class="accordion" @click="move(2)">
-        Форма<span class="accordion-arrow">&lsaquo;</span>
-      </button>
-      <div class="panel">
-        <p>Lorem ipsum...</p>
-      </div>
-
-      <button class="accordion" @click="move(3)">
-        Материал<span class="accordion-arrow">&lsaquo;</span>
-      </button>
-      <div class="panel">
-        <p>Lorem ipsum...</p>
-      </div>
-
-      <button class="accordion" @click="move(4)">
-        Тематика<span class="accordion-arrow">&lsaquo;</span>
-      </button>
-      <div class="panel">
-        <p>Lorem ipsum...</p>
-      </div>
-    </div>
-
-    <div class="content-products">
-      sdfsdfsdfs
-    </div> 
-
-    <!-- <div class="content-menu" id="catalog-menu">
-      <div class="arrow">&lsaquo;</div>
       <div class="content-accordion">
-
-        <div class="accordion-item">
-          <h3 class="accordion-title">Размер<span class="accordion-arrow">&lsaquo;</span></h3>
-          <div class="accordion-content">
-
-          </div>
-        </div>
-        
-         <div class="accordion-item">
-          <h3 class="accordion-title">Основной цвет<span class="accordion-arrow">&lsaquo;</span></h3>
-          <div class="accordion-content">
-            
-          </div>
-        </div>
-        
-         <div class="accordion-item">
-          <h3 class="accordion-title">Форма<span class="accordion-arrow">&lsaquo;</span></h3>
-          <div class="accordion-content">
-            
-          </div>
+        <div class="arrow">&lsaquo;</div>
+        <button class="accordion" @click="move(0)">
+          Размер<span class="accordion-arrow">&lsaquo;</span>
+        </button>
+        <div class="panel active">
+          <h4 class="panel-text">Кружки</h4>
+          <h4 class="panel-text">Высота от {} до {} мм</h4>
+          <input type="range" class="panel-height" step="1" min="0" max="130" @change.prevent="getValue">
         </div>
 
-         <div class="accordion-item">
-          <h3 class="accordion-title">Материал<span class="accordion-arrow">&lsaquo;</span></h3>
-          <div class="accordion-content">
-            
-          </div>
-        </div>
+        <button class="accordion" @click="move(1)">
+          Основной цвет<span class="accordion-arrow">&lsaquo;</span>
+        </button>
+        <div class="panel"></div>
 
-         <div class="accordion-item">
-          <h3 class="accordion-title">Тематика<span class="accordion-arrow">&lsaquo;</span></h3>
-          <div class="accordion-content">
-            
-          </div>
-        </div>
+        <button class="accordion" @click="move(2)">
+          Форма<span class="accordion-arrow">&lsaquo;</span>
+        </button>
+        <div class="panel"></div>
+
+        <button class="accordion" @click="move(3)">
+          Материал<span class="accordion-arrow">&lsaquo;</span>
+        </button>
+        <div class="panel"></div>
+
+        <button class="accordion" @click="move(4)">
+          Тематика<span class="accordion-arrow">&lsaquo;</span>
+        </button>
+        <div class="panel"></div>
       </div>
-    </div> -->
-    
+      
+    </div>
+    <div class="content-products">
+      <App-product-card v-for="item in mainCatalog" :item="item" :key="item"/>
+    </div>
   </div>
 </template>
 
 <script>
+import AppProductCard from './AppProductCard.vue';
+import {mainCatalog} from '../mocks/data'
 export default {
+  data() {
+    return {
+      mainCatalog
+    }
+  },
+  components: { AppProductCard },
   methods: {
     move(id) {
       let acc = document.getElementsByClassName("accordion");
@@ -100,11 +63,32 @@ export default {
         panel.style.display = "block";
       }
     },
+    getValue(event) {
+      console.log(event.target.value);
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
+
+.content-products{
+  padding: 40px 40px;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 2vw;
+}
+
+.panel-height{
+  width: 100%;
+}
+
+.panel-text{
+  margin-top: 12px;
+  margin-bottom: 12px;
+  font-size: 20px;
+}
 .accordion {
   border: none;
   position: relative;
@@ -114,25 +98,32 @@ export default {
   width: 100%;
   text-align: left;
   background-color: white;
+  justify-content: space-between;
   outline: none;
   transition: 0.4s;
   display: flex;
   user-select: none;
   font-size: 27px;
-  &:active {
-    .accordion-arrow {
-      color: #ed7102;
-    }
+  margin-top: 10px;
+}
+
+.accordion.active {
+  .accordion-arrow {
+    color: #b8b3ad;
+    transform: rotate(270deg);
+    transition-property: transform;
+    transition-duration: 0.8s;
   }
 }
 
-
 .panel {
+  text-align: center;
   padding: 0 18px;
   background-color: white;
   display: none;
   overflow: hidden;
 }
+
 
 .content {
   border-top: 1px solid #b8b3ad;
@@ -140,50 +131,31 @@ export default {
   display: flex;
 }
 
-// .content-menu {
-//   border-right: 1px solid #b8b3ad;
-//   position: relative;
-// }
 
 .content-products {
   border-left: 1px solid #b8b3ad;
 }
 
 .arrow {
+  user-select: none;
   position: absolute;
-  top: -10px;
+  top: -18px;
   right: 5px;
   color: #ed7102;
   font-weight: bold;
   font-size: 38px;
+  z-index: 10;
 }
 
-// .accordion-title {
-//   user-select: none;
-//   display: flex;
-//   font-size: 27px;
-//   &:active {
-//     .accordion-arrow {
-//       color: #ed7102;
-//     }
-//   }
-// }
-
-// .accordion-title .active {
-//   color: #b8b3ad;
-//   transform: rotate(180deg);
-// }
-
 .accordion-arrow {
-  display: block;
-  margin-left: 20px;
+  display: inline;
+  margin-left: 10px;
   color: #ed7102;
   transform: rotate(90deg);
 }
 
-// .accordion-item {
-//   cursor: pointer;
-//   padding-right: 20px;
-//   padding-top: 20px;
-// }
+.content-accordion {
+  position: relative;
+}
+
 </style>
