57e87fc7b92c1bbe887df3f2246b50c15052b9fb
Krllka <k.r.llaleks25@gmail.com>
2021-07-09 20:26:15 +0400

    update RestController

--- backend/src/main/java/app/controllers/RESTController.java
+++ backend/src/main/java/app/controllers/RESTController.java
@@ -2,10 +2,62 @@ package app.controllers;
 
 import app.controllers.dto.DTO;
 import app.services.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+*
+* @author Krll
+* */
 
 public abstract class RESTController<D extends DTO, S extends AbstractService<?, D, ?, ?>> {
 
-    S service;
+
+    @Autowired
+    protected S Service;
+
+    @GetMapping
+    public List<D> findAll() {
+        return Service.findAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<D> getBy(@PathVariable("id") Long id) {
+        return Service.findBy(id);
+    }
+    @PostMapping
+    public D save(@RequestBody D inputAcc ) {
+        try{
+            Service.save(inputAcc);
+            return inputAcc;
+        }catch (Exception ex){
+            return null;
+        }
+    }
+    @PutMapping("/{id}")
+    public D edit(@PathVariable("id") String id,   D edited) {
+
+        try{
+            Service.save(edited);
+            return  edited;
+        }catch (IllegalArgumentException ex){
+            return null;
+        }
+
+        return edited;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        try{
+            Service.deleteBy(id);
+
+        }catch(IllegalArgumentException ex){
+            return;
+        }
+    }
 
 
 }
--- backend/src/main/java/app/entities/AbstractEntity.java
+++ backend/src/main/java/app/entities/AbstractEntity.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-
+/**
+ * @author Krll
+ * */
 @Inheritance(strategy = InheritanceType.JOINED)
 @AllArgsConstructor
 @NoArgsConstructor
