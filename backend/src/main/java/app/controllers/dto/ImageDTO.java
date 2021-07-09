fb2f52595c24aba0e882c0edd0ccf2ef291b0bc6
Artem <footerka2@gmail.com>
2021-07-09 18:45:18 +0300

    ImageDTO

---
+++ backend/src/main/java/app/controllers/dto/ImageDTO.java
@@ -0,0 +1,50 @@
+package app.controllers.dto;
+
+import lombok.Data;
+import lombok.Value;
+/**
+ * @author ZubchenkoArtem
+ * */
+public enum ImageDTO {
+    ;
+
+    private interface Path {
+        String getPath();
+    }
+
+    private interface ImageDuty {
+        String getImageDuty();
+    }
+
+    public enum Request {
+        ;
+
+        @Data
+        public static class Get implements Path, ImageDuty {
+            String path;
+            String ImageDuty;
+
+        }
+
+        /**
+         * Создает свое изображение
+         */
+        @Data
+        public static class CreateCustom implements Path, ImageDuty {
+            private String path;
+            private String ImageDuty;
+
+        }
+
+        public enum Response {
+            ;
+
+            @Value
+            public static class GetImage implements Path, ImageDuty {
+                String path;
+                String ImageDuty;
+            }
+
+        }
+    }
+}
