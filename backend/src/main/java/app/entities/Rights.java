74f2eba34fb2c3c907e42cd83912ca75eb206dbb
Krllka <k.r.llaleks25@gmail.com>
2021-07-09 17:07:57 +0400

    fixed entities package

--- backend/src/main/java/app/entities/Rights.java
+++ backend/src/main/java/app/entities/Rights.java
@@ -6,15 +6,16 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@javax.persistence.Entity
+@Entity
 @Table
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class Rights extends Entity {
+public class Rights extends AbstractEntity {
 
     @Column
     private String name;
