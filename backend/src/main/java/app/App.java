15a68140be6deb2027736b79b30cf11c31e6df82
Artem <footerka2@gmail.com>
2021-07-09 17:05:20 +0300

    added Slot entity

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@EnableCaching(proxyTargetClass = true)
+@SpringBootApplication()
 public class App {
 
     public static void main(String[] args) {
