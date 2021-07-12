6aa63b23214d7f405a2eb38b82d9b1baa1062ff3
Ilya <180229@edu.psuti.ru>
2021-07-12 17:15:58 +0400

    Disable security

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -3,9 +3,10 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class App {
 
     public static void main(String[] args) {
