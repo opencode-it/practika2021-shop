152a9a22c109e8137b71e61d024f53bf9d5f6783
Ilya <180229@edu.psuti.ru>
2021-07-12 19:28:06 +0400

    Modify mappers

--- backend/src/main/java/app/mappers/impl/FeatureGetUnitsMapper.java
+++ backend/src/main/java/app/mappers/impl/FeatureGetUnitsMapper.java
@@ -3,7 +3,9 @@ package app.mappers.impl;
 import app.dto.impl.FeatureDTO;
 import app.entities.Feature;
 import app.mappers.AbstractMapper;
+import org.mapstruct.Mapper;
 
+@Mapper(uses = UnitsMapper.class)
 public interface FeatureGetUnitsMapper extends AbstractMapper<Feature,
                                                               FeatureDTO.Request.Get,
                                                               FeatureDTO.Response.GetWithUnits> {
--- backend/src/main/java/app/mappers/impl/FeatureUpdateAndGetUnitsMapper.java
+++ backend/src/main/java/app/mappers/impl/FeatureUpdateAndGetUnitsMapper.java
@@ -3,7 +3,9 @@ package app.mappers.impl;
 import app.dto.impl.FeatureDTO;
 import app.entities.Feature;
 import app.mappers.AbstractMapper;
+import org.mapstruct.Mapper;
 
+@Mapper(uses = UnitsMapper.class)
 public interface FeatureUpdateAndGetUnitsMapper extends AbstractMapper<Feature,
                                                                        FeatureDTO.Request.UpdateUnits,
                                                                        FeatureDTO.Response.GetWithUnits> {
