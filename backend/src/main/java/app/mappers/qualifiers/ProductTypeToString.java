7f61d248fb00de5db7026ec81704b3963c7eac4a
Ilya <180229@edu.psuti.ru>
2021-07-12 19:47:50 +0400

    Add qualifiers to mappers

--- backend/src/main/java/app/mappers/qualifiers/ProductTypeToString.java
+++ backend/src/main/java/app/mappers/qualifiers/ProductTypeToString.java
@@ -1,6 +1,6 @@
 package app.mappers.qualifiers;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.mapstruct.Qualifier;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
