bc50028ac87e2f163b6587c6dca26bc289a4abdd
Ilya <180229@edu.psuti.ru>
2021-07-12 19:28:06 +0400

    Modify mappers

--- backend/src/main/java/app/mappers/impl/ProductFilterAndGetBaseMapper.java
+++ backend/src/main/java/app/mappers/impl/ProductFilterAndGetBaseMapper.java
@@ -2,12 +2,35 @@ package app.mappers.impl;
 
 import app.dto.impl.ProductDTO;
 import app.entities.Product;
+import app.entities.ProductType;
 import app.mappers.AbstractMapper;
+import app.mappers.qualifiers.ProductTypeToString;
+import app.mappers.qualifiers.StringToProductType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(uses = FeatureUpdateAndGetUnitsMapper.class)
 public interface ProductFilterAndGetBaseMapper extends AbstractMapper<Product,
                                                                       ProductDTO.Request.FilterByFeatures,
                                                                       ProductDTO.Response.GetBase> {
 
+    @Mapping(target = "type", qualifiedBy = ProductTypeToString.class)
+    @Override
+    ProductDTO.Response.GetBase toDto(Product entity);
+
+    @Mapping(target = "type", qualifiedBy = StringToProductType.class)
+    @Override
+    Product toEntity(ProductDTO.Request.FilterByFeatures dto);
+
+    @ProductTypeToString
+    static String toString(ProductType productType) {
+        return productType.getName();
+    }
+
+    @StringToProductType
+    static ProductType toProductType(String name) {
+        return ProductType.builder()
+                .name(name)
+                .build();
+    }
 }
