8771179e11ea0941981e30d93388a31dc7145e41
Ilya <180229@edu.psuti.ru>
2021-07-13 20:08:39 +0400

    Add return stubs into controllers

--- backend/src/main/java/app/controllers/impl/ConstructorController.java
+++ backend/src/main/java/app/controllers/impl/ConstructorController.java
@@ -7,12 +7,7 @@ import app.dto.impl.ImageDTO;
 import app.dto.impl.ProductDTO;
 import app.services.ext.ProductEditAndGetFullService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -41,5 +36,6 @@ public class ConstructorController {
     public ImageDTO.Response.GetRequestedImage updateProductImage(
             @RequestBody ImageDTO.Request.CustomizeImage request) {
         //TODO
+        return null;
     }
 }
