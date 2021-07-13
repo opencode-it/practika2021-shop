f47296f7c27c4c47d5b0d12684d381f71de182a0
Ilya <180229@edu.psuti.ru>
2021-07-13 21:01:18 +0400

    Component scan fix

--- backend/src/main/java/app/App.java
+++ backend/src/main/java/app/App.java
@@ -3,10 +3,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan("app.mappers.impl.complex")
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class App {
 
