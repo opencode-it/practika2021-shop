43c6217dd164d98ba3b48f5b5f52b7b33ceccb1f
Krllka <k.r.llaleks25@gmail.com>
2021-07-14 03:03:42 +0400

    add up/downloading from minio to ImageController extend javadoc to RESTController and ImageController

--- backend/src/main/java/app/controllers/RESTController.java
+++ backend/src/main/java/app/controllers/RESTController.java
@@ -25,15 +25,28 @@ public abstract class RESTController<I extends RequestDTO,
 
     @Autowired
     protected S Service;
-
+    /**
+     * Метод, срабатывающтй при GET запросе, с путём ../../
+     * @return  Возвращает  список экземпляров {@link O}
+     */
     @GetMapping
     public List<O> findAll() {
         return Service.findAll();
     }
+    /**
+     * Метод, срабатывающтй при GET запросе, с путём ../../{id}
+     * @param id - ID объекта
+     * @return Возвращает экземпляров {@link O} по заданному id
+     */
     @GetMapping("/{id}")
     public Optional<O> getBy(@PathVariable("id") Long id) {
         return Service.find(id);
     }
+    /**
+     * Метод, срабатывающтй при POST запросе, с путём ../../{id}
+     * @param inputAcc - Экзмепляр {@link I} с даннми для загрузки в БД
+     * @return В случае успеха возвращает экземпляр inputAcc, иначе null
+     */
     @PostMapping
     public I save(@RequestBody I inputAcc ) {
         try{
@@ -42,9 +55,16 @@ public abstract class RESTController<I extends RequestDTO,
         }catch (Exception ex){
             return null;
         }
+
     }
+    /**
+     * Метод, срабатывающтй при PUT запросе, с путём ../../{id}
+     * @param id - ID сущности, в которорй необходимы изменения
+     * @param edited - эксемпляр {@link I}, с отредактированными данными для загрузки в БД
+     * @return В случае успеха возвращает экземпляр inputAcc, иначе null
+     */
     @PutMapping("/{id}")
-    public I edit(@PathVariable("id") String id, I edited) {
+    public I edit(@PathVariable("id") Long id, @RequestBody I edited) {
 
         try{
             Service.create(edited);
@@ -55,7 +75,10 @@ public abstract class RESTController<I extends RequestDTO,
 
 
     }
-
+    /**
+     * Метод, срабатывающтй при DELETE запросе, с путём ../../{id}
+     * @param id - ID сущности, в которую необходимо удалить
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Long id) {
         try{
