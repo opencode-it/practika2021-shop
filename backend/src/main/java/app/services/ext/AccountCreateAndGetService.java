0ce82b236eec68a7ee870befb9fc37220d1e7fb1
Ilya <180229@edu.psuti.ru>
2021-07-14 19:40:05 +0400

    Modify AccountCreateAndGetService

--- backend/src/main/java/app/services/ext/AccountCreateAndGetService.java
+++ backend/src/main/java/app/services/ext/AccountCreateAndGetService.java
@@ -4,6 +4,7 @@ import app.dto.impl.AccountDTO;
 import app.entities.Account;
 import app.mappers.impl.unidirectional.request.AccountCreateMapper;
 import app.mappers.impl.unidirectional.response.AccountGetFullInfoMapper;
+import app.mappers.impl.unidirectional.response.RightsGetMapper;
 import app.repositories.impl.AccountRepository;
 import app.repositories.impl.RightsRepository;
 import app.services.CRUDService;
@@ -23,4 +24,14 @@ public class AccountCreateAndGetService extends CRUDService<Account,
     @Autowired
     RightsRepository rightsRepository;
 
+    @Autowired
+    RightsGetMapper rightsMapper;
+
+    @Override
+    public AccountDTO.Response.GetFullInfo create(AccountDTO.Request.Create dto) {
+        rightsRepository.findById(dto.getRights().getId())
+                .ifPresent(r -> dto.setRights(rightsMapper.toDto(r)));
+        Account createdAccount = repository.save(requestMapper.toEntity(dto));
+        return responseMapper.toDto(createdAccount);
+    }
 }
