e55af6e4b3131ebcb25f85bb180ffedc9cc30d07
Ilya <180229@edu.psuti.ru>
2021-07-15 16:15:28 +0400

    Visit ready

--- backend/src/main/java/app/entities/Visit.java
+++ backend/src/main/java/app/entities/Visit.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -20,13 +17,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Visit extends AbstractEntity {
 
-    @ManyToOne()
+    @ManyToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name = "account_id")
+    @NotNull
     private Account account;
 
-    @ManyToOne()
+    @ManyToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name = "product_id")
+    @NotNull
     private Product reviewedProduct;
 
-    @NotNull
     @Column
+    @NotNull
     private LocalDateTime dateTime;
 }
