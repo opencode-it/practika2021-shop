63d043ee5448ec3887fb32cfa31a037ca863fda4
Ilya <180229@edu.psuti.ru>
2021-07-16 10:29:06 +0400

    Minor renaming

--- backend/src/main/java/app/services/ext/recommedations/ProductTypeRecommendationsService.java
+++ backend/src/main/java/app/services/ext/recommedations/ProductTypeRecommendationsService.java
@@ -1,4 +1,4 @@
-package app.services.ext.recommedation;
+package app.services.ext.recommedations;
 
 import app.algorithms.recommendations.impl.TypeRecommendations;
 import app.services.ext.ProductForAccountService;
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class ProductTypeRecommendationService extends ProductForAccountService<TypeRecommendations> {
+public class ProductTypeRecommendationsService extends ProductForAccountService<TypeRecommendations> {
 
 }
