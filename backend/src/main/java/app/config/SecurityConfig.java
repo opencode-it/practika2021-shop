4d43ea1db06f1a9e8ef86dc73f03fdc6b83fc08a
Krllka <k.r.llaleks25@gmail.com>
2021-07-16 10:32:14 +0400

    add AuthController add MvcConfig

---
+++ backend/src/main/java/app/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package app.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
---
+++ backend/src/main/java/app/controllers/impl/AuthController.java
@@ -0,0 +1,15 @@
+package app.controllers.impl;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+
+}
