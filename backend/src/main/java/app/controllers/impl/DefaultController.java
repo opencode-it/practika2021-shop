3679b79b1a0bf28db0af6f808e66ad4863c75b60
Krllka <k.r.llaleks25@gmail.com>
2021-07-19 15:16:15 +0400

    RIGHT VERSION 2.0 +optimize imports +update var names +added АУФ2 (почти)

--- backend/src/main/java/app/controllers/impl/DefaultController.java
+++ backend/src/main/java/app/controllers/impl/DefaultController.java
@@ -1,9 +1,6 @@
 package app.controllers.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ResolvableType;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Контроллер с приветствем :3
@@ -25,9 +20,7 @@ public class DefaultController {
 
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
-    Map oauth2AuthenticationUrls = new HashMap();
-    private static String authorizationRequestBaseUri
-            = "oauth2/authorization";
+
     @GetMapping("/")
     public String main(HttpServletResponse response) throws IOException {
         response.sendRedirect("/suc");
@@ -35,13 +28,6 @@ public class DefaultController {
     }
     @GetMapping("/suc")
     public String succes(){
-        Iterable clientRegistrations = null;
-        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
-                .as(Iterable.class);
-        if (type != ResolvableType.NONE &&
-                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
-            clientRegistrations = (Iterable) clientRegistrationRepository;
-        }
 
         return "suc";
     }
