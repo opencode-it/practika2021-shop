255bbfb9bafe3afa499b0bd5b02af9e5f53fecd5
Nezumish <monochromeworld42@gmail.com>
2021-07-19 12:13:56 +0400

    Небольшой рефакторинг 2.0

--- backend/src/main/java/app/controllers/RESTController.java
+++ backend/src/main/java/app/controllers/RESTController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 * */
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Log
 public abstract class RESTController<I extends RequestDTO,
                                      O extends ResponseDTO,
