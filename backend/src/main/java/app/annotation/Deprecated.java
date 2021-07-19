bd632ba189de60d9a10a628e02ff4c85fa27eb4e
Ilya <180229@edu.psuti.ru>
2021-07-19 15:21:24 +0400

    Add new annotation

---
+++ backend/src/main/java/app/annotation/Deprecated.java
@@ -0,0 +1,14 @@
+package app.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, MODULE, PARAMETER, TYPE})
+public @interface Deprecated {
+    String value() default "";
+}
